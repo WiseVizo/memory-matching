abf13b9c860c44b300e1345597f6dc4ae4555ab4
WiseVizo <mecoc1011@gmail.com>
2024-02-04 15:43:25 +0530

    ah bad but surely improved
    can't flip cards as i please for some reasons if i close a flipped card after that i can't flip new card with single click again

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
+import { useRef, useState } from "react";
 import "./App.css";
 import FlashCard from "./FlashCard/FlashCard";
 
 function App() {
+  // const [isFlipped1, setIsFlipped1] = useState(false);
+  // const [isFlipped2, setIsFlipped2] = useState(false);
+  const [selectedId1, setSelectedId1] = useState(-1);
+  const [isClicked, setIsClicked] = useState(false);
+  const selectedIdRef1 = useRef(selectedId1);
+  function handleSelectId1(id) {
+    setSelectedId1(() => {
+      console.log("before: " + selectedIdRef1.current);
+      selectedIdRef1.current = id;
+      console.log("after: " + selectedIdRef1.current);
+      return id;
+    });
+  }
   return (
     <div className="App">
       <h2>Pair All Cards With Same Contents To Win.</h2>
       <div className="body">
         {[...Array(9)].map((_, index) => (
-          <FlashCard key={index} />
+          <FlashCard
+            key={index}
+            index={index}
+            isClicked={isClicked}
+            setIsClicked={setIsClicked}
+            selectedId1={selectedId1}
+            handleSelectId1={handleSelectId1}
+            selectedIdRef1={selectedIdRef1}
+          />
         ))}
       </div>
     </div>
--- src/FlashCard/FlashCard.js
+++ src/FlashCard/FlashCard.js
@@ -1,17 +1,39 @@
-import { useState } from "react";
+// import { useState } from "react";
+// import { useEffect } from "react";
 import "./FlashCard.css";
 
-export default function Card() {
-  const [isClicked, setIsClicked] = useState(false);
-  function handleClick() {
-    setIsClicked(() => !isClicked);
+export default function Card({
+  isClicked,
+  setIsClicked,
+  index,
+  selectedId1,
+  handleSelectId1,
+  selectedIdRef1,
+}) {
+  function handleClick(index) {
+    handleSelectId1(index);
   }
+
+  function handleChange() {
+    console.log("hi: " + selectedIdRef1.current);
+    if (selectedIdRef1.current === index) {
+      setIsClicked(() => !isClicked);
+    }
+  }
+  // useEffect(() => {
+  //   setSelectedId(() => index);
+  // }, [index, setSelectedId]);
   return (
     <div
-      className={isClicked ? "main front" : "main back"}
-      onClick={handleClick}
+      className={
+        isClicked && index === selectedId1 ? "main front" : "main back"
+      }
+      onClick={() => {
+        handleClick(index);
+        handleChange();
+      }}
     >
-      {isClicked ? "front" : "back"}
+      {isClicked && index === selectedId1 ? "front" : "back"}
     </div>
   );
 }
